ebfb86cec363a51328a8fa2bc362c23c92081386
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-08-05 18:23:06 +0300

    feat: add initial redux structure

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# [`redux-cached-api-middleware`][RCAM] demo
+# [`RCAM`][RCAM] demo
 
 > redux-cached-api-middleware demo built with create-react-app
+
+## Installation
+
+`$ yarn`
+
+## Commands
+
+- `npm run start` - start a development server @ <http://localhost:3000/>
+- `npm run try-demo` - build all the assets and serve single page application @ <http://localhost:5000/>
 
 [RCAM]:https://www.npmjs.com/package/redux-cached-api-middleware
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
+import { store, persistor } from './state';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
